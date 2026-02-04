cb180b0347fa0f19d3b3d087d1031bfd49464e71
Darmostuk Aleksandr <sasha0darmostuk@gmail.com>
2026-02-05 00:16:57 +0300

    hw3

---
+++ src/containers/App.jsx
@@ -0,0 +1,13 @@
+import { Element, Container } from "../components/Components"
+import "../styles/styles.css"
+
+function App() {
+  return (
+    <>
+      <Container initialElements={[]}>
+      </Container>
+    </>
+  )
+}
+
+export default App
